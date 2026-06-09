13540a16ec9e8d4b4b59313e6e72fba937939c1b
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-10 11:49:59 +1200

    Called the class by creating the object named "employeeinfo". I have also added input functions to let users enter their own parameters. While also type casting some variables such as the number of employees and the salary. By calling other methods from the previous commits, I am able to add and search the information of employees into the system.

--- employee_salary_system.py
+++ employee_salary_system.py
@@ -41,3 +41,23 @@ class employeeSystem:
 
   def countEmployees(self):
     return len(self.__employees)
+  
+employeeinfo = employeeSystem()
+
+number_of_employees = int(input("Enter a valid number of employees that you would like to add: "))
+
+for i in range(number_of_employees):
+  print(f"\nEnter employee {i + 1}")
+
+  employee_name = input("Enter employee's name: ")
+  employee_position = input("Enter employee's position: ")
+  employee_salary = float(input("Enter employee's salary: "))
+
+  employeeinfo.addEmployees(employee_name, employee_position, employee_salary)
+  
+employeeinfo.displayEmployees()
+
+search_employee_name = input("\n Search for employee information in system: ")
+employeeinfo.searchEmployee(search_employee_name)
+
+print(f"\n Total employees: {employeeinfo.countEmployees()}") 
