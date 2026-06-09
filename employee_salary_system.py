a912fb82bef4f5c341e1f05e1f419177708a733b
Ethan Andrei Rivera <ethanrivera0409@gmail.com>
2026-06-10 11:26:39 +1200

    Added methods to display, and search employees. Used a combination of python functions to check if an employee exists in the list such as if else statements, and the len string method. Used for loop for both methods to display the information like employee name, position and salary while also going through the list to search for a certain employee. I also added return to return the values.

--- employee_salary_system.py
+++ employee_salary_system.py
@@ -3,8 +3,35 @@ class employeeSystem:
   def __init__(self): #constructor
     self.__employees = [] #list for all the employees and their information
 
-  def addEmployee(self, employee_name, employee_position, employee_salary):
+  def addEmployees(self, employee_name, employee_position, employee_salary):
     employee = {"employee name": employee_name, "employee position": employee_position, "employee salary": employee_salary}
 
     self.__employees.append(employee)
     print(f"{employee_name}, has been added to the system.")
+
+  def displayEmployees(self):
+    if len(self.__employees) == 0:
+      print("Employee has been found in the system.")
+
+    else:
+      print("\nEmployee Salary System")
+      print("---------------")
+
+      for employee in self.__employees:
+        print(f"Employee Name: {employee["employee name"]}")
+        print(f"Employee Position: {employee["employee position"]}")
+        print(f"Employee Salary: {employee["employee salary"]}")
+
+        print("---------------")
+
+  def searchEmployee(self, employee_name):
+    for employee in self.__employees:
+      if employee["Employee Name"].lower() == employee_name.lower():
+        print("\nEmployee found in the system.")
+        print(f"Employee Name: {employee["employee name"]}")
+        print(f"Employee Position: {employee["employee position"]}")
+        print(f"Employee Salary: {employee["employee salary"]}")
+
+        return
+      
+      print("Employee not found in the system.")
